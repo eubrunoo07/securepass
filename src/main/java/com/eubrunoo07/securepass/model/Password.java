7eac4c97b687d854f665dad77150d60803adf399
eubrunoo07 <devbruninn@outlook.com>
2024-09-03 16:49:56 -0300

    Creating password model

---
+++ src/main/java/com/eubrunoo07/securepass/model/Password.java
@@ -0,0 +1,21 @@
+package com.eubrunoo07.securepass.model;
+
+import com.eubrunoo07.securepass.enums.PasswordLevel;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "password_tb")
+@Data
+public class Password {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private PasswordLevel passwordLevel;
+
+}
