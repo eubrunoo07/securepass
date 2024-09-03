d9fae8f09e1b69aa5c3f31495b9b2e0254588a04
eubrunoo07 <devbruninn@outlook.com>
2024-09-03 16:30:52 -0300

    first commit for project

---
+++ src/main/java/com/eubrunoo07/securepass/SecurePasswordForMeApplication.java
@@ -0,0 +1,13 @@
+package com.eubrunoo07.securepass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurePasswordForMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurePasswordForMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eubrunoo07/securepass/SecurePasswordForMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eubrunoo07.securepass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurePasswordForMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
