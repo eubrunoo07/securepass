6b8398bd0fbe8c8de5e6c4c8d263357f670fec0a
eubrunoo07 <devbruninn@outlook.com>
2024-09-03 16:42:55 -0300

    Creating user model

---
+++ src/main/java/com/eubrunoo07/securepass/model/User.java
@@ -0,0 +1,22 @@
+package com.eubrunoo07.securepass.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "user_tb")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column
+    private String username;
+    @Column
+    private String email;
+    @Column
+    private String password;
+
+}
